dc3af188629f8735bed1170d3092f2661f3355e6
Wendy Osiris Rivas Martinez <wrivas@ahorra.io>
2023-09-28 13:55:28 -0600

    rutas

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  linkActiveClass:"active",
   routes: [
     {
       path: '/',
@@ -24,6 +25,11 @@ const router = createRouter({
       path: '/pokemons/:name',
       name: 'poke',
       component: () => import('../views/PokeView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)',
+      name: 'NotFound',
+      component: () => import('../views/NotFoundView.vue')
     }
   ]
 })
--- src/views/PokeView.vue
+++ src/views/PokeView.vue
@@ -1,36 +1,47 @@
 <script setup>
-import axios from 'axios';
-import { useRoute } from 'vue-router';
-import { ref } from 'vue';
+import axios from "axios";
+import { useRoute, useRouter } from "vue-router";
+import { ref } from "vue";
 
 const route = useRoute();
-const pokemon =ref({})
-const getData = async()=>{
-    try{
-        console.log(`${route.params.name}`, "rutas");
-        const {data}= await axios.get( `https://pokeapi.co/api/v2/pokemon/${route.params.name}`)
-        pokemon.value = data
-        console.log(data);
-        
-    }catch(error){
-console.log(error);
-    }
+const router = useRouter();
+const pokemon = ref({});
+const getData = async () => {
+  try {
+    console.log(`${route.params.name}`, "rutas");
+    const { data } = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${route.params.name}`
+    );
+    pokemon.value = data;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    pokemon.value = null;
+  }
 };
-getData()
+const back=()=>{
+router.push("/pokemons");
+}
+getData();
 </script>
 <template>
-    <div>
-     <h1>
-        nombre pokemon:{{ $route.params.name }}
-    </h1>
+  <div class="container-fluid">
+    <div v-if="pokemon">
+      <h1>nombre pokemon:{{ $route.params.name }}</h1>
       <div class="card" style="width: 18rem;">
-        <img :src="`${pokemon.sprites.front_default}`" class="card-img-top" alt="...">
+        <img :src="`${pokemon.sprites.front_default}`" class="card-img-top" alt="..." />
         <div class="card-body">
           <h5 class="card-title">{{pokemon.species.name}}</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p
+            class="card-text"
+          >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
-      </div>    
+      </div>
     </div>
-   
+    <div v-else>
+      <h1>No existe en pokemon</h1>
+    </div>
+    <button @click="back" class="btn btn-outline-primary">Volver</button>
+  </div>
 </template>
